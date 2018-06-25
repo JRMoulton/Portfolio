6f5e8b77d9049017d745c6821955d52bf8b85f74
Jared Moulton <jaredmoulton3@gmail.com>
2018-06-24 21:39:32 -0600

    Working play button (not in right place)

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
     $("#first").css("height", ($(window).height()).toString() + "px")
 });
+
+$(".fa-play-circle").click(function() {
+    if ($("#video").get(0).paused) {
+        $("#video").get(0).play();
+        $(".fa-play-circle").animate({fontSize: "180%"}, "fast");
+    } else {
+        $("#video").get(0).pause();
+        $(".fa-play-circle").animate({fontSize: "150%"}, "fast");
+    }
+})
